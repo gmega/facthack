5db0d45ac5df4ecbc4108b3d2b1cc5e887fcbf5c
Ubuntu <ubuntu@ip-172-31-26-10.eu-west-1.compute.internal>
2016-04-22 13:11:40 +0000

    add filtering to dedup :-/

--- indices/dedup.py
+++ indices/dedup.py
@@ -4,8 +4,17 @@ import sys
 seen = set()
 
 for raw_document in sys.stdin:
+    # Skip empty lines.
+    if not raw_document.strip():
+        continue
     document = json.loads(raw_document)
     hash = document['nc:contentInfo']['nc:hash']
+    
+    # Filters out articles without company mentions.
+    if not document['nc:annotations'].get('nc:subjectAnnotation', []):
+        continue
+
     if hash not in seen:
         seen.add(hash)
-        json.dump(hash, sys.stdout)
+        json.dump(document, sys.stdout)
+        sys.stdout.write('\n')
