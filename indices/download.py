cc5e096ffe21e090bb900f162b05ea3cd7507036
gmega <mega@spaziodati.eu>
2016-04-22 11:43:27 +0200

    add filtering

--- indices/download.py
+++ indices/download.py
@@ -1,12 +1,67 @@
+# -*- coding: utf-8 -*-
+
 import json
 import sys
 
 from elasticsearch import helpers
 from elasticsearch.client import Elasticsearch
 
+EXCLUDE_SOURCES = [
+    'Noodls', 'The Sidney Morning Herald.com', 'Reuters UK', 'Usa Today.com', 'Stern.de', 'Facts.ch',
+    'CalcioMercato.com', 'Herald Scotland.com', 'CalcioMercato.it', 'Calcio Fanpage',
+    'The Washington Post.com', 'Basketinside.com', 'Thestar.com', 'TheGuardian', 'BBC News',
+    'La Gazzetta dello Sport (Ed. Roma)', 'The Japan Times', 'La Gazzetta dello Sport.it',
+    'Frankfurter Allgemeine.faz.net', 'Le Monde.fr', 'La Gazzetta dello Sport', 'ChinaDaily.com.cn',
+    'La Nacion', '20 Minuten.ch', 'Bloomberg', 'Libération.fr', 'Adevarul.ro', 'Dagensindustri.se',
+    'Corriere dello Sport.it', 'Cinco Dìas', 'Datasport', 'ZDNet.com', 'The Telegraph.com.a',
+    'Sport Asti.it', 'COR.COM', 'Janes.com', 'Ok Tennis', 'ZDNet.de', 'Tutto Basket.net',
+    'La Gazzetta dello Sport (Ed. Sicilia)', 'FinYear.com', 'The Telegraph.co.uk',
+    'La Gazzetta dello Sport (Ed. Puglia)', 'Sport Press', 'SportReggio', 'Food Navigator.com',
+    'Tuttosport.com', 'Catholic News Service.com', 'Le Monde Diplomatique', 'Classic Boat.co.uk',
+    'Ve.Sport.it', 'Digital Agenda for Europe', 'Milano online'
+]
 
-def filter_document(document):
-    return document
+FILTER_CATEGORIES = ['Cronaca Locale', 'Prima Pagina', 'Economia / Finanza', 'Non specificata']
+
+FILTER_SECTORS = ['Generalista', 'Attualità', 'Economia e Finanza']
+
+EXCLUDE_HASHES = ['0647ac34aa1ed17ba998f7fc1d1f7ef2', 'd41d8cd98f00b204e9800998ecf8427e']
+
+
+def should_drop(document):
+    info = document['nc:contentInfo']
+
+    body_size = info['nc:bodySize']
+    body_dirt = info['nc:bodyDirt']
+    empty_title = info['nc:emptyTitle']
+    borsa = info['nc:listOfNamesNumbers']
+    hash = info['nc:hash']
+    # lang_qual = info['nc:langQuality']
+
+    categories = get_categories(document)
+    #    source = get_source(document)
+
+    return body_dirt >= 0.5 or \
+           body_size < 300 or \
+           body_size > 50000 or \
+           hash in EXCLUDE_HASHES or \
+           any(category in FILTER_CATEGORIES for category in categories) or \
+           empty_title or \
+           borsa
+
+
+def get_source(document):
+    extras = document['nc:extras'].get('nc:presstoday', None)
+    # This is a Noodl (pressrelease)
+    if extras is None:
+        return None
+
+    return extras['source']
+
+
+def get_categories(document):
+    categories = document['nc:annotations']['nc:categoryAnnotation'].get('pt:category', [])
+    return [category['nc:categoryID'] for category in categories]
 
 
 def process_index(index, client, output):
@@ -18,9 +73,9 @@ def process_index(index, client, output):
             doc_type='news_v2',
             scroll='5m'
     ):
-        processed = filter_document(document)
-        if processed:
-            json.dump(document['_source'], output)
+        document = document['_source']
+        if not should_drop(document):
+            json.dump(document, output)
         output.write('\n')
 
 
@@ -28,6 +83,7 @@ client = Elasticsearch(sys.argv[1])
 
 with open('index-sample.csv', 'r') as indices:
     for index in indices:
+        index = index.strip()
         with open('./outputs/%s.jsonl' % index, 'w') as output:
-            process_index(index.strip(), client, output)
+            process_index(index, client, output)
         break
