06f22bc27837a850c269b883254500ec48c7e84c
gmega <mega@spaziodati.eu>
2016-04-22 15:16:52 +0200

    add news filters to dedup

--- indices/dedup.py
+++ indices/dedup.py
@@ -3,6 +3,10 @@ import sys
 
 seen = set()
 
+def is_good(annotation):
+    threshold = 0.7 if annotation['nc:reconciled'] else 0.8
+    return annotation['nc:confidence'] >= threshold
+
 for raw_document in sys.stdin:
     # Skip empty lines.
     if not raw_document.strip():
@@ -10,8 +14,13 @@ for raw_document in sys.stdin:
     document = json.loads(raw_document)
     hash = document['nc:contentInfo']['nc:hash']
     
-    # Filters out articles without company mentions.
-    if not document['nc:annotations'].get('nc:subjectAnnotation', []):
+    # Filters out bad annotations.
+    annotations = [
+        x for x in document['nc:annotations'].get('nc:subjectAnnotation', []) if is_good(x)
+    ]
+
+    # If nothing is left (or nothing was there in the first place), discards the document.
+    if not annotations:
         continue
 
     if hash not in seen:
